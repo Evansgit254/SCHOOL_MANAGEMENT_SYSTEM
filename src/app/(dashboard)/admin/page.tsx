b66b1f5bfe8c9a8c5d4eb380ce9515da885e1a47
Evans Mumo <evansmumo665@gmail.com>
2025-02-18 17:04:57 +0300

    first commit

---
+++ src/app/(dashboard)/admin/page.tsx
@@ -0,0 +1,35 @@
+import CountChart from '@/components/CountChart'
+import UserCard from '@/components/UserCard'
+import React from 'react'
+
+const AdminPage
+ = () => {
+  return (
+    <div className='p-4 flex gap-4 flex-col  md:flex-row '>
+      {/* LEFT */}
+      <div className='w-full lg:w-2/3 flex flex-col gap-8'>l</div>
+      {/* USER CARDS */}
+      <div className='flex gap-4 flex-col lg:flex-row flex-wrap'>
+        <UserCard type='student'/>
+        <UserCard type='teacher'/>
+        <UserCard type='parent'/>
+        <UserCard type='staff'/>
+      </div>
+      {/* MIDDLE CHARTS */}
+      <div className='flex gap-4 flex-col lg:flex-row '>
+        {/* COUNT  CHART */}
+        <div className='w-full lg:w-1/3 h-[450px]'>
+          <CountChart />
+        </div>
+        {/* ATTENDANCE CHART */}
+        <div className='w-full lg:w-2/3 h-[450px]'></div>
+      </div>
+      {/* BOTTOM CHARTS */}
+      <div className=''></div>
+      {/* RIGHT */}
+      <div className='w-full lg:w-1/3'>r</div>
+    </div>
+  )
+}
+
+export default AdminPage
